76501f6b8ad99f599d71c409a5b1bd3c85f37c87
Makosai <kristopher.ali.dev@gmail.com>
2024-07-12 17:29:51 -0400

    Update app.

--- index.pck
+++ index.pck
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ea50b57c955f9e3cce9994feb73c0dd7c8be075d8e6c0acc0bf0210d5e8f32c
-size 221747264
+oid sha256:e5f0914f069f9272195325bf2c75c39f355afab2f2e5fa41046a9d7af51464c6
+size 221690400
--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1720736087|241122852";
+const CACHE_VERSION = "1720747310|11464138278";
 const CACHE_PREFIX = "Reia-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
